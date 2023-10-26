d7cb20f49ded419347888875849b783012a00bda
Orestis-Antonopoulos <skyscreamer@live.com>
2023-10-26 07:33:06 +0300

    Added to graphical interface

--- index.php
+++ index.php
@@ -5,7 +5,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>I can't think of a title at 4:30am</title>
     <style>
-        body {font-family: 'Roboto', sans-serif;}
+    body {font-family: 'Roboto', sans-serif; 
+        color:#999;
+        background-color: rgb(220, 227, 255);
+        padding-top:60px;
+        animation:backgroundAnimation 20s infinite;}
+    @keyframes backgroundAnimation {
+        0% {background-color: rgb(183, 197, 201);}
+        50% {background-color: rgb(209, 217, 255);}
+        100% {background-color: rgb(183, 197, 201);}
+        }
+    /* waves animation start */
+        .ocean {
+        height: 80px; /* change the height of the waves here */
+        width: 100%;
+        position: fixed;
+        z-index: 10;
+        top: 0;
+        left: 0;
+        right: 0;
+        transform: rotate(180deg);
+        overflow-x: hidden;
+        pointer-events: none;
+
+        }
+
+        .wave {
+        background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 800 88.7'%3E%3Cpath d='M800 56.9c-155.5 0-204.9-50-405.5-49.9-200 0-250 49.9-394.5 49.9v31.8h800v-.2-31.6z' fill='%23FFF'/%3E%3C/svg%3E");
+        position: absolute;
+        width: 200%;
+        height: 100%;
+        animation: wave 7s -3s linear infinite;
+        transform: translate3d(0, 0, 0);
+        opacity: 0.9;
+        z-index: 2;
+        }
+
+        .wave:nth-of-type(2) {
+        bottom: 0;
+        animation: wave 9s linear reverse infinite;
+        opacity: 0.5;
+        z-index: 2;
+        }
+
+        .wave:nth-of-type(3) {
+        bottom: 0;
+        animation: wave 11s -1s linear infinite;
+        opacity: 0.5;
+        z-index: 2;
+        }
+
+        @keyframes wave {
+            0% {transform: translateX(0);}
+            50% {transform: translateX(-16%);}
+            100% {transform: translateX(-33%);}
+        }
+    /* waves animation end */
         th {color:#700;}
         td {
             background-color:#eee;
@@ -21,7 +76,7 @@
         .tooltiptext {
             visibility: hidden;
             position:absolute;
-            z-index: 1;
+            z-index: auto;
             background-color:#eee;
             padding:5px;
             /* border: solid black 1px; */
@@ -35,7 +90,7 @@
         .tooltiptext2 {
             visibility: hidden;
             position:absolute;
-            z-index: 1;
+            z-index: auto;
             background-color:#eee;
             padding:5px;
             /* border: solid black 1px; */
@@ -57,17 +112,29 @@
             display:flex;
             justify-content: center;
             gap:10px;
+            position:fixed;
+            z-index: 10;
+            width:100%;
+            top:10px;
         }
         .button {
             width:80px;
             height:25px;
-            clip-path: polygon(10px 0%, 100% 0, 100% 15px, 70px 25px, 0 100%, 0 10px);
+            /* clip-path: polygon(10px 0%, 100% 0, 100% 15px, 70px 25px, 0 100%, 0 10px); */
             border:none;
-            background-color:#bcc8d3;
+            border-radius:5px;
+            background-color:rgb(100% 100% 100% / 0.2);
+            box-shadow: 0px 2px 5px 0 rgb(0 0 0 / 0.2);
 
         }
         .active {
-            background-color:#a1a8ed;
+            box-shadow: none;
+            animation:active-animation 10s infinite;
+        }
+        @keyframes active-animation {
+        0% {background-color: rgb(93% 93% 93% / 0.7);}
+        50% {background-color: rgb(90% 90% 93% / 1);}
+        100% {background-color: rgb(93% 93% 93% / 0.7);}
         }
 
         @media (min-width: 768px) and (max-width: 1024px) {
@@ -80,6 +147,11 @@
     </style>
 </head>
 <body>
+<div class="ocean">
+  <div class="wave"></div>
+  <div class="wave"></div>
+  <div class="wave"></div>
+</div>
 <div id="buttons">
     <input type="button" class="button" data-table="table3" name="Button3" value="CPU" onclick="showTable('table3', this)" />
     <input type="button" class="button" data-table="table2" name="Button2" value="System" onclick="showTable('table2', this)" />
